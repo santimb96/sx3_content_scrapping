0af3fd1d7b041374d74b3f3c17c89f7c33d3f9f0
santimb96 <santiagomartinezbota@gmail.com>
2022-10-12 16:03:25 +0200

    some functionalities added: the script checks if the path exist, if the file exist in the path and some controls about the server response

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from msilib.schema import Patch
 import os
 import requests
 import json
@@ -8,13 +9,14 @@ from bs4 import BeautifulSoup
 def app():
 
     load_dotenv()
-    URL_BASE = requests.get(os.getenv('URL_BASE'))
-    API_URL = os.getenv('API_URL')
+
+    URL_BASE = requests.get(os.getenv("URL_BASE"))
+    API_URL = os.getenv("API_URL")
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
-        }
-    html_page = ''
-    chapters_list = ''
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
+    }
+    html_page = ""
+    chapters_list = ""
     video_id = []
     videos = []
 
@@ -26,22 +28,63 @@ def app():
 
     def get_media_links(list):
         for chapter in list:
-            for link in chapter.findAll("a"):
-                video_id.append(link.get("href").split("/")[7])
+            link = chapter.findAll("a")
+            video_id.append(link[0]["href"].split("/")[7])
         return
 
-    def get_data_from_link(ids):
-        for id in ids:
-            req = requests.get(f"{API_URL}{id}", headers=headers)
-            data = json.loads(req.text)
-            videos.append(data["media"]["url"][0]["file"])
+    def get_data_from_link(ids=[]):
 
+        if len(ids):
+            for id in ids:
+                try:
+                    req = requests.get(f"{API_URL}{id}", headers=headers)
+                    data = json.loads(req.text)
+                    videos.append(
+                        {
+                            "title": data["informacio"]["titol"],
+                            "url": data["media"]["url"][0]["file"],
+                        }
+                    )
+                except:
+                    raise Exception("¡Something went wrong!")
+        else:
+            print("¡The video's id list is empty!")
+            return quit()
         return
-        
+
+    def check_drive_exist(drive):
+        return os.path.exists(drive + ":\\")
+
+    def download_videos(videos):
+        directory = "E:\\" if check_drive_exist("e") else "C:\\"
+        path = os.path.join(directory, "videos")
+
+        if not os.path.exists(path):
+            os.mkdir(path)
+
+        for index, video in enumerate(videos):
+            file_name = str(index) + "-" + video["title"] + ".mp4"
+            file_path = os.path.join(path, file_name)
+
+            if not os.path.exists(file_path):
+                
+                req = requests.get(video["url"], stream=True)
+                print("The file doesn't exist ---> Downloading " + video["title"])
+    
+                with open(file_path, "wb") as f:
+                 for chunk in req.iter_content(chunk_size=1024 * 1024):
+                     if chunk:
+                         f.write(chunk)
+    
+                print("File downloaded!")
+
+        return print("¡All files downloaded!")
+
     html_page = page_parser(URL_BASE.content)
     chapters_list = get_chapter_url(html_page)
     get_media_links(chapters_list)
     get_data_from_link(video_id)
+    download_videos(videos)
 
 
 app()
